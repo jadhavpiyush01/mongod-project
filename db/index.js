e58050687abbad30a1ae512f4aebab639c50cd9c
jadhavpiyush01 <164189368+jadhavpiyush01@users.noreply.github.com>
2024-03-24 18:24:30 -0700

    g

--- db/index.js
+++ db/index.js
@@ -1,11 +1,27 @@
-import { MongoClient } from "mongodb";
-const connectionString = process.env.ATLAS_URI || "";
-const client = new MongoClient(connectionString);
-let conn;
-try {
-  conn = await client.connect();
-} catch(e) {
-  console.error(e);
+const moongoose = require("mongoose")
+const dbUrl = "mongodb+srv://lokesh-kabra:lokesh12@cluster0.bmw0v.mongodb.net/wae-workshop"
+
+async function db() {
+    moongoose.connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log('Connected to MongoDB'))
+        .catch((error) => console.error('MongoDB connection error:', error));
+    // try {
+    //     const mongodb = await moongoose.connect(dbUrl, {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //         useCreateIndex: true
+    //     })
+    //     if (mongodb) {
+    //         console.log("Connected to MongoDB")
+    //     }
+    // } catch (error) {
+    //     console.error('MongoDB connection error:', error)
+    // }
+
+
 }
-let db = conn.db("sample_training");
-export default db;
+
+module.exports = db;
